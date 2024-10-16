2fa0236c373e5cd400c52d0e7acb2bc53ba3d710
jerichosy <69558553+jerichosy@users.noreply.github.com>
2024-10-16 22:37:21 +0800

    Revert "(bot token): Put it in application.yml instead"
    This reverts commit 9e7083acd61b4f82ef03b751d0c437ac20f0ca1f.

--- src/main/java/com/novamaday/d4j/gradle/springbot/SpringBot.java
+++ src/main/java/com/novamaday/d4j/gradle/springbot/SpringBot.java
@@ -5,7 +5,6 @@ import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.presence.ClientActivity;
 import discord4j.core.object.presence.ClientPresence;
 import discord4j.rest.RestClient;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
@@ -19,12 +18,10 @@ public class SpringBot {
             .run(args);
     }
 
-    @Value("${discord.bot.token}")
-    private String botToken;
 
     @Bean
     public GatewayDiscordClient gatewayDiscordClient() {
-        return DiscordClientBuilder.create(botToken).build()
+        return DiscordClientBuilder.create(System.getenv("BOT_TOKEN")).build()
             .gateway()
             .setInitialPresence(ignore -> ClientPresence.online(ClientActivity.custom("\uD83D\uDE80 has better internet than Wai")))
             .login()
