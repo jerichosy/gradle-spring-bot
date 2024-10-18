704e4d59a61a6f2b96b96143c6cf058c5b12ef2f
zen-young <renzcruz50@gmail.com>
2024-10-19 01:30:25 +0800

    refactor: remove dependency on service

--- src/main/java/com/novamaday/d4j/gradle/springbot/commands/RemindMeCommand.java
+++ src/main/java/com/novamaday/d4j/gradle/springbot/commands/RemindMeCommand.java
@@ -1,6 +1,7 @@
 package com.novamaday.d4j.gradle.springbot.commands;
 
-import com.novamaday.d4j.gradle.springbot.services.ReminderService;
+import com.novamaday.d4j.gradle.springbot.entities.Reminder;
+import com.novamaday.d4j.gradle.springbot.repositories.ReminderRepository;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import discord4j.core.object.command.ApplicationCommandInteractionOption;
 import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
@@ -11,12 +12,13 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class RemindMeCommand implements SlashCommand {
+
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    private final ReminderService reminderService;
+    private final ReminderRepository reminderRepository;
 
-    public RemindMeCommand(ReminderService reminderService) {
-        this.reminderService = reminderService;
+    public RemindMeCommand(ReminderRepository reminderRepository) {
+        this.reminderRepository = reminderRepository;
     }
 
     @Override
@@ -42,12 +44,11 @@ public class RemindMeCommand implements SlashCommand {
 
         long triggerTime = System.currentTimeMillis() + parseTimeStringToMillis(timeString);
 
-        reminderService.saveReminder(
+        reminderRepository.save(new Reminder(
             event.getInteraction().getUser().getId().asLong(),
             event.getInteraction().getChannelId().asLong(),
             message,
-            triggerTime
-        );
+            triggerTime));
 
         LOGGER.info("Saved reminder for user id {}", event.getInteraction().getUser().getId().asString());
 
@@ -58,12 +59,13 @@ public class RemindMeCommand implements SlashCommand {
 
     private long parseTimeStringToMillis(String timeString) {
         try {
+            long millis = Integer.parseInt(timeString.substring(0, timeString.length() - 1));
             if (timeString.endsWith("s")) {
-                return Integer.parseInt(timeString.substring(0, timeString.length() - 1)) * 1000L;
+                return millis * 1000L;
             } else if (timeString.endsWith("m")) {
-                return Integer.parseInt(timeString.substring(0, timeString.length() - 1)) * 60_000L;
+                return millis * 60_000L;
             } else if (timeString.endsWith("h")) {
-                return Integer.parseInt(timeString.substring(0, timeString.length() - 1)) * 3_600_000L;
+                return millis * 3_600_000L;
             } else {
                 return Long.parseLong(timeString) * 1000L;
             }
