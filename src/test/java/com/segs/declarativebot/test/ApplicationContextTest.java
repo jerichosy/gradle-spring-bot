56ef814667f0c293ebd725a28da130b6151ac47e
zen-young <renzcruz50@gmail.com>
2024-10-21 11:40:13 +0800

    test: add ApplicationContext test

---
+++ src/test/java/com/segs/declarativebot/test/ApplicationContextTest.java
@@ -0,0 +1,12 @@
+package com.segs.declarativebot.test;
+
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationContextTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
