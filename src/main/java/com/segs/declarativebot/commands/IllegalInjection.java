836b87bdecd465367f41127a754acaf25b9a202d
jerichosy <69558553+jerichosy@users.noreply.github.com>
2025-02-04 18:33:28 +0800

    refactor(illegal injection cmd): Remove temp URL params in Discord image link

--- src/main/java/com/segs/declarativebot/commands/IllegalInjection.java
+++ src/main/java/com/segs/declarativebot/commands/IllegalInjection.java
@@ -18,7 +18,7 @@ public class IllegalInjection implements Command {
     public Mono<Void> execute(MessageCreateEvent event) {
         EmbedCreateSpec embed = EmbedCreateSpec.builder()
             .title("Wai:")
-            .image("https://cdn.discordapp.com/attachments/1178014261243551929/1296147657580085279/AP1GczOMTM_u6LdYfpw3oTOrTV63w4C7Z9uiEa5TxYbnaTSpNsB_mL2Qb4A6HAw1270-h1270-s-no-gm.png?ex=67113b12&is=670fe992&hm=a8644082b595d3d492a26f7afdcc81a88bcc7b9e93d94a36ca1b3edd9cb39d18&")
+            .image("https://cdn.discordapp.com/attachments/1178014261243551929/1296147657580085279/AP1GczOMTM_u6LdYfpw3oTOrTV63w4C7Z9uiEa5TxYbnaTSpNsB_mL2Qb4A6HAw1270-h1270-s-no-gm.png")
             .build();
 
         MessageCreateSpec message = MessageCreateSpec.builder()
